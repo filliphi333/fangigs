5166fd2c19e862b54a423ea79aac8f05f75fcf5c
Filliphi <filliphi@icloud.com>
2025-06-04 18:54:41 -0500

    Fix Tailwind paths after src folder removal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -11,4 +13,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
+
